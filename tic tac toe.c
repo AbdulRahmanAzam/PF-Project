b0d9e52eba6c37bea547e06f916da39e8fc8b751
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-11-18 21:02:58 +0500

    Update tic tac toe.c

--- tic tac toe.c
+++ tic tac toe.c
@@ -97,3 +97,37 @@ int main() {
             }
     return 0;
 }
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#include <stdio.h>
+void tictac(int arr[9]){
+    for(int i=0;i<9;i++){
+        printf(" %d " ,arr[i]);
+        if(i%3 <2) printf("|");
+        if((i+1)%3==0){
+            printf("\n");
+            if(i<7) for(int j=0;j<3;j++)printf(" -- ");
+            printf("\n");
+        }
+    }
+}
+
+int main(){
+    int n=1;
+    int game;
+    int arr[9] = {1,2,3,4,5,6,7,8,9};
+    printf("Wanted to play  multiplayer or single \n Enter 1 for multiplayer and 0 for single ");
+    scanf("%d",&game);
+    if(game){
+        
+    }else{
+        
+    }
+    tictac(arr);
+    
+}
+
