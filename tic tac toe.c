5852624150f5f95d86a9b0a8d518b86be7ed3b7f
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-11-26 22:38:27 +0500

    Update tic tac toe.c

--- tic tac toe.c
+++ tic tac toe.c
@@ -3,27 +3,24 @@
 
 #define SIZE 3
 
-// int isPosValid(char board[SIZE][SIZE],int row,int col){
-// 	if(row >= SIZE || col >= SIZE || row < 0 || col < 0){
-// 		return 0;
-// 	}else if(board[row][col] == '-'){
-// 		return 1;
-// 	}
-// }
-
+//to print the board of tic tac toe
 void printBoard(char board[SIZE][SIZE]){
 	for(int i=0; i < SIZE; i++){
 		for(int j =0 ; j < SIZE; j++){
-			printf(" %c ", board[i][j]);
+			if(j < SIZE-1) printf(" %c |", board[i][j]);
+			else printf(" %c ", board[i][j]);
 		}
 		printf("\n");
+		if(i < SIZE -1) for (int k=0; k < SIZE; k++) printf(" --"); // ------------------------------------------------------------
+		printf("\n"); // -----------------------------------------------------------------------------------------------------------
 	}
 }
 
+//check whether the board have available inputs or not
 int isBoardFull(char board[SIZE][SIZE]){
 	for(int i=0;i < SIZE; i++){
 		for(int j=0;j <SIZE; j++){
-			if(board[i][j] == '-'){
+			if(board[i][j] >= '1' && board[i][j] <= '9'){ // -----------------------------------------------------------------------------------------------------------
 				return 0; // board is not full
 			}
 		}
@@ -31,6 +28,7 @@ int isBoardFull(char board[SIZE][SIZE]){
 	return 1; // board is full
 }
 
+// to check whether player wins or not
 int winner(char board[SIZE][SIZE], char ch){
 	for(int i=0;i < SIZE; i++){
 		if ((board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] == ch ) ||
@@ -47,6 +45,15 @@ int winner(char board[SIZE][SIZE], char ch){
     return 0;
 }
 
+// to check whether the move is valid or it is already input before
+int isValid(char board[SIZE][SIZE], int row, int col){
+	if(board[row][col] >= '1' && board[row][col] <= '9'){
+		return 1; // yes position valid
+	}
+	return 0; // position not valid 
+}
+
+// evaluation funciton used for minimax algorithm
 int evaluate(char board[SIZE][SIZE]){
 	if(winner(board, 'X')){
 		return 1; // computer wins
@@ -59,21 +66,23 @@ int evaluate(char board[SIZE][SIZE]){
 	}
 }
 
+//minimax algorithm which is recursive function, calculates the best input by checking the whole game (ARTIFICIAL INTELLIGENCE)
 int minimax(char board[SIZE][SIZE], int depth, bool isMax){
 	int score = evaluate(board);
 	if(score != 2){
 		return score;
 	}
-
+	char ch;
 	if(isMax){
 		int max = -1000;
 		for(int i=0;i < SIZE; i++){
 			for(int j=0; j < SIZE; j++){
-				if(board[i][j] == '-'){
+				if(isValid(board, i, j)){ //// -----------------------------------------------------------------------------------------------------------
+					ch = board[i][j]; // -----------------------------------------------------------------------------------------------------------
 					board[i][j] = 'X';
 					int current = minimax(board,depth + 1, !isMax);
 					max = (current > max) ? current : max;
-					board[i][j] = '-';
+					board[i][j] = ch; // -----------------------------------------------------------------------------------------------------------
 				}
 			}
 		}
@@ -82,11 +91,12 @@ int minimax(char board[SIZE][SIZE], int depth, bool isMax){
 		int min = 1000;
 		for(int i=0;i < SIZE; i++){
 			for(int j=0;j < SIZE; j++){
-				if(board[i][j] == '-'){
+				if(isValid(board, i, j)){// -----------------------------------------------------------------------------------------------------------
+					ch = board[i][j];// -----------------------------------------------------------------------------------------------------------
 					board[i][j] = 'O';
 					int current = minimax(board, depth + 1, !isMax);
 					min = (current < min) ? current : min;
-					board[i][j] = '-';
+					board[i][j] = ch; // -----------------------------------------------------------------------------------------------------------
 				}
 			}
 		}
@@ -94,17 +104,20 @@ int minimax(char board[SIZE][SIZE], int depth, bool isMax){
 	}
 }
 
+// the turn of the computer which calls minimax algo to find its input
 void computer(char board[SIZE][SIZE]){
 	int best = -1000;
 	int row = -1, col = -1;
+	char ch;
 
 	for(int i=0;i < SIZE; i++){
 		for(int j=0;j < SIZE; j++){
 
-			if(board[i][j] == '-'){
+			if(isValid(board, i, j)){ // -----------------------------------------------------------------------------------------------------------
+				ch = board[i][j]; // -----------------------------------------------------------------------------------------------------------
 				board[i][j] = 'X';
 				int move = minimax(board, 0, 0);
-				board[i][j] = '-';
+				board[i][j] = ch;// -----------------------------------------------------------------------------------------------------------
 
 				if(move > best){
 					row = i;
@@ -114,16 +127,17 @@ void computer(char board[SIZE][SIZE]){
 			}
 		}
 	}
+	printf("computer plays move: %d", 3*r -1);
 	board[row][col] = 'X';
 }
 
+//when the player is playing with computer
 void single(char board[SIZE][SIZE]){
+	printBoard(board);
 	while(1){
-		printf("current board\n");
-		printBoard(board);
 		int move;
 		int row,col;
-		printf("Enter your move");
+		printf("Enter your move ");
 		scanf("%d",&move);
 		
 		row = (move - 1) / 3;
@@ -148,6 +162,8 @@ void single(char board[SIZE][SIZE]){
 
 		computer(board);
 
+		printBoard(board);
+
 		if(winner(board, 'X')){
 			printf("Computer wins\n ");
 			break;
@@ -160,19 +176,67 @@ void single(char board[SIZE][SIZE]){
 	}
 	return;
 }
+// Two player game
+void multiplayer(char board[SIZE][SIZE]){
+	char player = 'X';
+	int move;
+	int row,col;
+	while(1){
+		printBoard(board);
+		printf("player %c enter move ",player);
+		scanf("%d", &move);
+		row = (move - 1) / 3;
+		col = (move - 1) % 3;
 
-int main(){
-	char board[SIZE][SIZE] = {
-		{'-','-','-'},
-		{'-','-','-'},
-		{'-','-','-'}
-	};
-	single(board);
+		if(isValid(board,row,col)){
+			if(player == 'X'){
+				board[row][col] = 'X';
+			}else if(player == 'O'){
+				board[row][col] = 'O';
+			}
+
+		}else{
+			printf("Invalid input, Enter correct move\n");
+			player = (player == 'X') ? 'O' : 'X';
+		}
+
+		if(winner(board,player)){
+			printf("Congratulations player %c won the game!\n", player);
+			return;
+		} 
+		if(isBoardFull(board)){
+			printf("Game is Draw\n");
+			return;
+		}
+
+		player = (player == 'X') ? 'O' : 'X';
+	}
 }
 
+int main(){
+	int cont;
+	int level;
+	int mode;
+	char board[SIZE][SIZE] = {
+		{'1','2','3'},
+		{'4','5','6'},
+		{'7','8','9'}
+	};
+	printf("WELCOME TO TIC TAC BOARD BATTLE\n\n");
+	do{
+		printf("Choose the mode of the game \nDo you want to play multiplayer or single\nEnter 1 for multiplayer or 0 for single\n");
+		scanf("%d",&mode);
 
-// char board[SIZE][SIZE] = {
-// 		{'1','2','3'},
-// 		{'4','5','6'},
-// 		{'7','8','9'}
-// 	}
+		if(mode){
+			multiplayer(board);
+		}else{
+			single(board);		
+		}
+
+		printf("Do you want to play again\n Enter 1 to continue or Enter 0 to exit\n");
+		scanf("%d", &cont);
+	}while(cont);
+	printf("See you again!");
+	
+}
+
